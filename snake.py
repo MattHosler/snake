3a2a9708d9188e30a22acec47ac9c02ff6a6c673
MattHosler <MatthewCHosler@gmail.com>
2022-12-16 17:27:06 -0500

    Added part 2

--- snake.py
+++ snake.py
@@ -42,9 +42,29 @@ title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 score_text = font.render("Score: " + str(score), True, GREEN, DARKRED)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10,10)
-#Set sound and music
 
-#Set images
+game_over_text = font.render("GAME OVER", True, RED, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 +64)
+
+#Set sound and music
+pick_up_sound = pygame.mixer.Sound("pick_up_sound.wav")
+
+#Set images (In this case use simple rects so just create their coordinates)
+#For a rectangle you need (top left x, top left y, width, height)
+apple_coord =(500, 500, SNAKE_SIZE, SNAKE_SIZE)
+apple_rect = pygame.draw.rect(display_surface, GREEN, apple_coord)
+
+head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+head_rect = pygame.draw.rect(display_surface, RED, head_coord)
+
+body_coords = []
+
+
 
 
 #The main game loop
@@ -55,5 +75,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    display_surface.fill(WHITE)
+    #Blit the hud
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(score_text, score_rect)
+
+    #Blit assets
+    #Still need to do body 
+    pygame.draw.rect(display_surface, GREEN, head_coord)
+    pygame.draw.rect(display_surface, RED, apple_coord)
+
+    #Update display and tick clock
+    pygame.display.update()
+    clock.tick(FPS)
 #End the game
 pygame.quit()
